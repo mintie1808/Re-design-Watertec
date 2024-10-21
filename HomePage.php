eff24910e2230fab64b9bc93b8c5e9770f200072
NeitHnim <111961232+mintie1808@users.noreply.github.com>
2024-10-21 16:09:44 +0700

    Initial commit

---
+++ HomePage.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./asset/style.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
+        rel="stylesheet">
+    <title>Document</title>
+</head>
+
+
+<body>
+    <div class="container-fluid">
+        <div class="w-80 mx-auto">
+            <div class="home-header">
+                <div class="navbar home-header-nav">
+                    <div class="header-logo"><img src="asset/img/logoWatertec.png" width="146.778px" height=" 59.161px"
+                            flex-shrink: 0 /></div>
+                    <div class="container-menu">
+                        <ul class="navbar menu">
+                            <li><span>Home</span></li>
+                            <li><span>About US</span></li>
+                            <li><span>Products</span></li>
+                            <li><span>News</span></li>
+                            <li><span>Contact</span></li>
+                            <li><span>Servicing & Support</span><img src="asset/img/down-arrow.png" width="25px"
+                                    height="25px" /></li>
+                        </ul>
+                    </div>
+                    <div class="language">
+                        <img src="asset/img/language.png" alt="" width="20px" height="20px">
+                        <select>
+                            <option value=""><span>EN</span></option>
+                            <option value=""><span>VI</span></option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="content-about">
+                <div class="container-heading">
+                    <div class="heading-about">
+                        <div class="text-about">ABOUT</div>
+                        <div class="text-watertec"><span class="text-water">WATER</span><span
+                                class="text-tec">TEC</span></div>
+                        <div class="text-subtitle">WATERTEC EXCLUSIVE DISTRIBUTOR IN VIETNAM</div>
+                    </div>
+                    <div class="info-about ">
+                        <div class="container-about-text">Watertec is the first Malaysian company to lead the way in
+                            inventing and manufacturing plastic valves, faucets & fittings in Asia. Watertec's plastic
+                            faucets are made of high quality processed plastic with a warranty of up to 10 years.
+                            Production output is more than 6 million units annually.<br /><br />
+                            Watertec products are present in ASEAN countries, the Middle East and Africa. Currently,
+                            Watertec is the largest manufacturer of valves, faucets and plastic accessories in the
+                            world. Phan Khang Home Company only specializes in providing high quality bathroom & kitchen
+                            equipment.</div>
+                        <div class="container-about-img">
+                            <img src="asset/img/background_about.png" width="457px" height="266px" />
+                            <div class="about-img">
+                                <ul class="container-about-img-detail">
+                                    <li> <img src="asset/img/about_detail1.png" alt="" width="108px" height="63px"></li>
+                                    <li> <img src="asset/img/about_detail2.png" alt="" width="108px" height="63px"></li>
+                                    <li> <img src="asset/img/about_detail3.png" alt="" width="108px" height="63px"></li>
+                                    <li> <img src="asset/img/about_detail4.png" alt="" width="108px" height="63px"></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-about">
+                    <div class="footer-about-info-head d-flex justify-content-center align-items-center"><span
+                            style="margin-right: 10px;">Watertec is the </span> <span
+                            style="margin-right: 10px; color: #005498;">leading </span> <span> plastic valve, faucet &
+                            fittings market in:</span></div>
+                    <div class="footer-about-flag d-flex justify-content-center align-items-center">
+                        <ul>
+                            <li>
+                                <img src="asset/img/image 23.png" alt="" class="img-flag">
+                            </li>
+                            <li> <img src="asset/img/image 24.png" alt="" class="img-flag"></li>
+                            <li><img src="asset/img/image 25.png" alt="" class="img-flag"></li>
+                            <li><img src="asset/img/image 26.png" alt="" class="img-flag"></li>
+                            <li><img src="asset/img/image 27.png" alt="" class="img-flag"></li>
+                            <li><img src="asset/img/image 28.png" alt="" class="img-flag"></li>
+                            <li><img src="asset/img/image 29.png" alt="" class="img-flag"></li>
+                            <li><img src="asset/img/image 30.png" alt="" class="img-flag"></li>
+                            <li><img src="asset/img/image 31.png" alt="" class="img-flag"></li>
+                            <li><img src="asset/img/image 32.png" alt="" class="img-flag"></li>
+                            <li><img src="asset/img/image 33.png" alt="" class="img-flag"></li>
+                            <li><img src="asset/img/image 34.png" alt="" class="img-flag"></li>
+                        </ul>
+                    </div>
+                    <div class="footer-about-info-foot d-flex justify-content-center align-items-center"><span>and many
+                            more!</span></div>
+                </div>
+            </div>
+            <div class="home-content-product">
+                <div class="home-heading-product">
+                    <span class="text-our">Our</span></br>
+                    <span class="text-product">Products</span>
+                </div>
+                <div class="home-container-product">
+                    <div class="container-product">
+                        <div class="container-img-product"><span>All</br> Products</span></div>
+                    </div>
+                    <div class="container-product">
+                        <div class="container-img-product"><span>Pillar Sink</br> Tap</span></div>
+                    </div>
+                    <div class="container-product">
+                        <div class="container-img-product"><span>Wall Sink</br> Tap</span></div>
+                    </div>
+                    <div class="container-product">
+                        <div class="container-img-product"><span>Hand Bidet</span></div>
+                    </div>
+                    <div class="container-product">
+                        <div class="container-img-product"><span>Stop Valve</span></div>
+                    </div>
+                    <div class="container-product">
+                        <div class="container-img-product"><span>Basin Tap</span></div>
+                    </div>
+                    <div class="container-product">
+                        <div class="container-img-product"><span>Hand</br> Shower</span></div>
+                    </div>
+                    <div class="container-product">
+                        <div class="container-img-product"><span>Hose</br> Connect</span></div>
+                    </div>
+                    <div class="container-product">
+                        <div class="container-img-product"><span>Bib Tap</span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="home-content-news">
+                <div class="home-heading-news">
+                    <div class="home-cont-text-news">
+                        <span class="text-about">Latest</span></br>
+                        <span class="text-news">News</span>
+                    </div>
+                </div>
+                <div class="home-cont-slider-news">
+                    <div class="home-slider-news">
+                        <div class="home-news-details">
+                            
+                        </div>
+                    </div>
+                    <div>
+                        ...
+                    </div>
+                </div>
+
+            </div>
+            <div class="content-4">
+
+            </div>
+            <div class="footer">
+
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
