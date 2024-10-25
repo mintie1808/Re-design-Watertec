3d26c3a1725a08e12881d26cd1758dd67700a042
NeitHnim <111961232+mintie1808@users.noreply.github.com>
2024-10-25 14:23:50 +0700

    product-details

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
+        rel="stylesheet">
     <title>Your Website Name - Home</title>
 </head>
 
@@ -16,19 +20,13 @@
     <div class="container-fluid">
         <div class="mx-auto">
             <?php 
-                include './page/homePage.php';
+               // include './page/homePage.php';
                 // include './page/aboutPage.php';
-                // Example of dynamic page inclusion
-                // $currentPage = 'home'; // This can be changed based on your routing logic
-                // if ($currentPage === 'home') {
-                //     include './page/homePage.php';
-                // } elseif ($currentPage === 'about') {
-                //     include './page/aboutPage.php';
-                // }
+                include './page/detaiProductPage.php'
             ?>
         </div>
     </div>
-    
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
